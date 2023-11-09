d08d35257bc133cfd40f993f766f05d32a6958e0
Rubengd93 <rubengd93@gmail.com>
2023-11-09 19:29:45 +0100

    Bucles While

---
+++ buclesWhile.ts
@@ -0,0 +1,44 @@
+
+function hasEven(myNums:number[]) {
+
+let i = 0;
+
+    do { 
+        if (myNums[i] % 2 === 0) {
+
+            console.log(myNums[i]);
+            
+        }
+
+        i++;
+        
+    } while (i <= myNums.length);
+    
+}
+
+let array = [3, 2, 6, 8, 10, 11];
+
+console.log(hasEven(array));
+
+
+
+function startWithM(myNames:string[]) {
+
+    let i = 0;
+    
+
+    do {
+        if (myNames[i][0].toUpperCase() === "M") {
+            
+            return true;
+            
+        }
+        i++;
+        
+    } while (i < myNames.length);
+    
+}
+
+let namesArray1 = ["Maria", "Miguel", "Monica"];
+console.log(startWithM(namesArray1));
+
